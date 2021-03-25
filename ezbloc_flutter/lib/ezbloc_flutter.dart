1641f551d5e0c7794277e8468849b15844fad94a
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2021-03-25 02:19:35 +0100

    remove dependency on flutter_test

--- ezbloc_flutter/lib/ezbloc_flutter.dart
+++ ezbloc_flutter/lib/ezbloc_flutter.dart
@@ -4,6 +4,7 @@ export 'package:ezbloc/src/bloc.dart';
 export 'package:ezbloc/src/bloc_monitor.dart';
 export 'package:ezbloc/src/persisted_bloc.dart';
 export 'package:ezbloc/src/persistence_service.dart';
+export 'package:ezbloc/src/bloc_test_.dart';
 export 'src/bloc_builder.dart';
 export 'src/bloc_container.dart';
 export 'src/bloc_list_builder.dart';
