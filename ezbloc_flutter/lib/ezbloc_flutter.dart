fbd9934ab2ccd11cae009626ab9edb3468140064
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2023-04-20 22:23:15 +0200

    export flutter bloc instead of pure dart one

--- ezbloc_flutter/lib/ezbloc_flutter.dart
+++ ezbloc_flutter/lib/ezbloc_flutter.dart
@@ -1,10 +1,6 @@
 library ezbloc_flutter;
 
-export 'package:ezbloc/src/bloc.dart';
-export 'package:ezbloc/src/bloc_monitor.dart';
-export 'package:ezbloc/src/persisted_bloc.dart';
-export 'package:ezbloc/src/persistence_service.dart';
-export 'package:ezbloc/src/bloc_test_.dart';
+export 'package:ezbloc/ezbloc.dart' hide Bloc;
 export 'src/bloc_builder.dart';
 export 'src/bloc_container.dart';
 export 'src/bloc_list_builder.dart';
@@ -13,3 +9,4 @@ export 'src/bloc_extensions.dart';
 export 'src/bloc_test_.dart';
 export 'src/bloc_provider.dart';
 export 'src/context_extensions.dart';
+export 'src/bloc.dart';
