f85a7faf006dab6dcc37102eaa756aa6193074f0
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2023-04-25 22:00:25 +0200

    add comments

--- ezbloc/lib/src/bloc_event_type.dart
+++ ezbloc/lib/src/bloc_event_type.dart
@@ -1,7 +1,16 @@
 enum BlocEventType {
+  /// [Bloc] is initialized
   init,
+
+  /// [Bloc] encountered an error
   error,
+
+  /// State is changed
   stateChange,
+
+  /// [Bloc] is busy
   busy,
+
+  /// [Bloc] has no listeners (stream is being closed)
   streamClosed,
 }
