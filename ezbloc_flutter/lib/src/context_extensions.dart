a9ca55b085561c67ab197bca452b53cec6ee0afe
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2023-07-04 22:13:27 +0200

    add extensions to get state and bloc from context

--- ezbloc_flutter/lib/src/context_extensions.dart
+++ ezbloc_flutter/lib/src/context_extensions.dart
@@ -8,11 +8,31 @@ import 'bloc_extensions.dart';
 extension ContextX<S> on BuildContext {
   bool get isDisposed => !mounted;
 
-  S? blocValue<R extends Bloc<S>>() {
-    final bloc = BlocProvider.of<R>(this);
-    return bloc.state;
+  /// Get a [Bloc] that exists in an ancestor widget
+  ///
+  /// returns null if none is found
+  R? bloc<R extends Bloc<S>>() {
+    return BlocProvider.of<R>(this);
   }
 
+
+  /// Get the current [Bloc.state] from a [Bloc] that exists in an ancestor
+  /// widget
+  ///
+  /// returns null if none is found
+  S? blocState<R extends Bloc<S>>() {
+    final bloc = BlocProvider.of<R>(this);
+    final hasState = bloc?.hasState ?? false;
+    return hasState ? bloc!.state : null;
+  }
+
+  @Deprecated('use [blocState]')
+  S? blocValue<R extends Bloc<S>>() {
+    final bloc = BlocProvider.of<R>(this);
+    return bloc?.state;
+  }
+
+  @Deprecated('use [bloc.builder]')
   Widget blocBuilder<R extends Bloc<S>>({
     Key? key,
     required DataBuilder<S> onState,
@@ -20,7 +40,7 @@ extension ContextX<S> on BuildContext {
     ErrorBuilder? onError,
   }) {
     final bloc = BlocProvider.of<R>(this);
-    return bloc.builder(
+    return bloc!.builder(
       key: key,
       onState: onState,
       onBusy: onBusy,
